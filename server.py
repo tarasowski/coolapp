eb46e09f4d49b529d7419f858efb7f46f34b979c
Dimi Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-02-06 11:00:16 +0100

    feat: added a feature flag

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+# a simple feature flag example
+weatherEndpointActive = False
 
 @app.route('/health', methods=['GET'])
 def health_check():
@@ -8,12 +10,13 @@ def health_check():
     return jsonify({"status": "OK"}), 200
 
 
-@app.route('/weather', methods=['GET'])
-def health_check():
-    # here we should can a weather API to retrieve the weather of a city
-    # and return the weather to the client
-    # this is a fake example 👇
-    return jsonify({"city": "Stuttgart", "temparature": "25", "unit": "celcius"}), 200
+if (weatherEndpointActive):
+    @app.route('/weather', methods=['GET'])
+        def health_check():
+            # here we should can a weather API to retrieve the weather of a city
+            # and return the weather to the client
+            # this is a fake example 👇
+            return jsonify({"city": "Stuttgart", "temparature": "25", "unit": "celcius"}), 200
 
 
 if __name__ == '__main__':
