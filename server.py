4df76a114b455fb177914ee66857a79f903edffb
Dimi Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-02-06 10:58:35 +0100

    feat: added weather endpoint

--- server.py
+++ server.py
@@ -7,5 +7,14 @@ def health_check():
 
     return jsonify({"status": "OK"}), 200
 
+
+@app.route('/weather', methods=['GET'])
+def health_check():
+    # here we should can a weather API to retrieve the weather of a city
+    # and return the weather to the client
+    # this is a fake example 👇
+    return jsonify({"city": "Stuttgart", "temparature": "25", "unit": "celcius"}), 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
