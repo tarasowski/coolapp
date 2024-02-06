f25395f892e65217d5a6bdeb5bce71ea2618254c
Dimi Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-02-06 09:40:43 +0100

    fix: fixed the message for the health endpoint

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/health', methods=['GET'])
 def health_check():
-    return jsonify({"status": "OK"}), 200
+    return jsonify({"status": "GOODNEWCHANGE"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
